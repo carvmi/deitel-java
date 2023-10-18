92a7b70e0cbb69bf13e5553ff000d77f79118f14
Milena <105459743+carvmi@users.noreply.github.com>
2023-10-18 14:19:11 -0300

    Create Welcome4.java

---
+++ praticas/Welcome4.java
@@ -0,0 +1,11 @@
+// Exibindo múltiplas linhas com o método System.out.printf.
+3
+4 public class Welcome4
+5 {
+6 // método main inicia a execução do aplicativo Java
+7 public static void main(String[] args)
+8 {
+9 ïSystem.out.printf("%s%n%s%n", ÿ
+10 ï "Welcome to", "Java Programming!");ÿ
+11 } // fim do método main
+12 } // fim da classe Welcome4
