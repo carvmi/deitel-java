ceff771c63ead8ee8b70131874bdf6f67e46aef8
Milena <105459743+carvmi@users.noreply.github.com>
2023-10-19 15:44:44 -0300

    Update Welcome3.java

--- praticas/Welcome3.java
+++ praticas/Welcome3.java
@@ -1,10 +1,10 @@
 // Imprimindo múltiplas linhas de texto com uma única instrução.
-3
-4 public class Welcome3
-5 {
-6 // método main inicia a execução do aplicativo Java
-7 public static void main(String[] args)
-8 {
-9 System.out.println("Welcomeï\nÿtoï\nÿJavaï\nÿProgramming!");
-10 } // fim do método main
-11 } // fim da classe Welcome3
+
+ public class Welcome3
+{
+ // método main inicia a execução do aplicativo Java
+public static void main(String[] args)
+ {
+System.out.println("Welcome\nto\nJava\nProgramming!");
+ } // fim do método main
+} // fim da classe Welcome3
